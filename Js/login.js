2cae208997d816cd92320b95dbdcf890b1f7ebc9
rolle324 <jeremiasahonen@hotmail.com>
2022-12-08 00:45:47 +0200

    testaus ympäristö muutettu Jest

--- Js/login.js
+++ Js/login.js
@@ -3,8 +3,8 @@ const loginButton = document.querySelector('#loginButton');
 let loggedIn = false;
 
 // Add login function to the login button
-$(document).ready(function () {
-    $("#loginButton").click(function () {
+$(document).ready(function() {
+    $("#loginButton").click(function() {
         login();
 
     });
@@ -12,9 +12,18 @@ $(document).ready(function () {
 
 
 
-async function login() {
-    let userName = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
+
+function login(testuser, testpass) {
+    let userName;
+    let password;
+
+    if (testuser && testpass != null) {
+        userName = testuser;
+        password = testpass;
+    } else {
+        userName = document.getElementById('username').value;
+        password = document.getElementById('password').value;
+    }
 
     const data = {
         "username": userName,
@@ -22,16 +31,21 @@ async function login() {
     }
 
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:8080/api/login");
+    xhr.open("POST", "http://localhost:8080/api/login", false);
     xhr.setRequestHeader("Accept", "application/json");
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onload = function () {
+    xhr.onload = function() {
         console.log("xhr " + xhr.responseText);
         if (xhr.responseText == "true") {
             console.log("Bool toimii");
             loggedIn = true;
             console.log("loginjs loggedIn: " + loggedIn);
-            window.location.href = "../html/main.html";
+            if (testuser && testpass != null) {
+                console.log("testi suoritettu");
+            } else {
+                window.location.href = "../html/main.html";
+            }
+
 
 
         } else {
@@ -45,4 +59,9 @@ async function login() {
 
     xhr.send(eventString);
 
+    return xhr.responseText;
 }
+
+
+
+module.exports = login
---
+++ Js/tests/favorite.test.js
@@ -0,0 +1,31 @@
+const testFavorite = require('../favorite');
+const loginbefore = require('../login');
+
+
+//Logins before doing any tests
+beforeAll(() => {
+    loginbefore("rulla", "joku22");
+});
+
+test("Movie/series should be able to be added in to the database", () => {
+
+    const data = {
+        "name": "testi",
+        "rate": 8.0,
+        "posterpath": "testi",
+        "test": true
+
+    }
+
+    let response = testFavorite(1, data);
+    expect(response).toBe('true')
+
+
+})
+
+test("Movie/series should be able to be removed in to the database", () => {
+
+    let response = testFavorite(2, "testi");
+    expect(response).toBe('true')
+
+})
