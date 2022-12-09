a86ef84e44a6411ec94736070cf547bcc0d09628
rolle324 <jeremiasahonen@hotmail.com>
2022-12-09 14:06:47 +0200

    Sprint 7 search/Advanced search ja roulette toiminnot valmiit

--- Js/main.js
+++ Js/main.js
@@ -6,26 +6,26 @@
 let logged = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-
-	if (logged) {
-		console.log("show buttons");
-		showButtons();
-	} else {
-		console.log("hide buttons");
-		hideButtons();
-	}
+    console.log("hih");
+    if (logged) {
+        console.log("show buttons");
+        showButtons();
+    } else {
+        console.log("hide buttons");
+        hideButtons();
+    }
 
 });
 
 
 function hideButtons() {
-	document.getElementById("loginA").style.display = "none";
-	document.getElementById("registerA").style.display = "none";
+    document.getElementById("loginA").style.display = "none";
+    document.getElementById("registerA").style.display = "none";
 
 }
 
 function showButtons() {
-	document.getElementById("loginA").style.display = "unset";
-	document.getElementById("registerA").style.display = "unset";
+    document.getElementById("loginA").style.display = "unset";
+    document.getElementById("registerA").style.display = "unset";
 
 }
